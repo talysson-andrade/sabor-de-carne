85905770313d400cca109f6f575ec9629a2a752d
Talysson <talysson.and@gmail.com>
2023-11-03 00:44:18 -0300

    Adicionando Produtos ao cardápio com JS

--- script/cardapio.js
+++ script/cardapio.js
@@ -1,27 +1,55 @@
+var produtos = [
+    {
+        imagem: '/images/Lanche-01.jpeg',
+        nome: 'Hambúguer Bom',
+        descricao: 'Hambúrguer bom, gostoso que só',
+        preco: 'R$ 19,90'
+    },
+    {
+        imagem: '/images/Lanche-02.jpeg',
+        nome: 'Hambúguer Médio',
+        descricao: 'Hamburguer médio, mais ou menos gostoso',
+        preco: 'R$ 14,99'
+    },
+    {
+        imagem: '/images/Lanche-03.jpeg',
+        nome: 'Hambúguer Médio',
+        descricao: 'Hamburguer médio, mais ou menos gostoso',
+        preco: 'R$ 14,99'
+    },
+    {
+        imagem: '/images/Lanche-04.jpeg',
+        nome: 'Hambúguer Médio',
+        descricao: 'Hamburguer médio, mais ou menos gostoso',
+        preco: 'R$ 14,99'
+    },
+    {
+        imagem: '/images/Lanche-05.jpeg',
+        nome: 'Hambúguer Médio',
+        descricao: 'Hamburguer médio, mais ou menos gostoso',
+        preco: 'R$ 14,99'
+    },
+    {
+        imagem: '/images/milkshake.jpeg',
+        nome: 'Hambúguer Médio',
+        descricao: 'Hamburguer médio, mais ou menos gostoso',
+        preco: 'R$ 14,99'
+    },
 
+]
 
-const respTbl = document.createElement("div");
-const tblBody = document.createElement("div");
-const tblRow = document.createElement("div");
-const tblCell = document.createElement("div");
+    var tabela = document.getElementById('tabelaProdutos').getElementsByTagName('tbody')[0];
 
-respTbl.setAttribute("id", "resp-table");
-tblBody.setAttribute("id", "resp-table-body");
-tblRow.setAttribute("id", "resp-table-row");
-tblCell.setAttribute("id", "table-body-cell");
+    for (var i = 0; i < produtos.length; i++){
+        var produto = produtos[i];
+        var row = tabela.insertRow(i);
+        var cell1 = row.insertCell(0);
+        var img = document.createElement('img');
+        img.src = produto.imagem;
+        img.alt = produto.nome;
+        cell1.appendChild(img);
 
-respTbl.appendChild(tblBody);
-tblBody.appendChild(tblRow);
-
-var links = document.querySelectorAll('a');
-
-links.forEach(function(link) {
-link.addEventListener('click', function(event) {
-event.preventDefault();
-
-var destino = link.getAttribute('href');
-
-window.location.href = destino;
-});
-});
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = '<h2>' + produto.nome + '</h2><p>'+produto.descricao + '</p><p>Preço: ' + produto.preco + '</p>';
+    }
 
